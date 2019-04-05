afcb8418bc127a53c7f613183249967313453cd6
RagePeanut <dorian.gruselin@hotmail.com>
2019-04-05 22:01:07 +0200

    Fix: double html insert when going from profile to settings

--- background.js
+++ background.js
@@ -4,11 +4,11 @@ chrome.tabs.onUpdated.addListener(urlUpdated);
  * Sends a message to the content scripts if the page is ready to be edited.
  * 
  * @param {number} tabId The tab id
- * @param {object} _changeInfo The changes to the state of the tab that was updated
+ * @param {object} changeInfo The changes to the state of the tab that was updated
  * @param {object} tab The state of the tab that was updated.
  */
-function urlUpdated(tabId, _changeInfo, tab) {
-    if(tab.status == "complete") {
+function urlUpdated(tabId, changeInfo, tab) {
+    if(tab.status == "complete" && !changeInfo.favIconUrl) {
         if(tab.url == "https://steemit.com/submit.html" && tab.title == "Create a post — Steemit") {
             chrome.tabs.sendMessage(tabId, "editor");
         } else if(/https:\/\/steemit\.com\/@[a-z\d.-]{3,16}\/settings/.test(tab.url) && /.* \(@[a-z\d.-]{3,16}\) — Steemit/.test(tab.title)) {
